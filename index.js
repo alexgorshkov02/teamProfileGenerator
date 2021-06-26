d104751beb38d94a6dbd04714be267672f074c0d
Alex <82488910+alexgorshkov02@users.noreply.github.com>
2021-06-26 18:49:48 -0400

    Added finish method. Code refactoring. Issue #8

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const PageGenerator = require('./lib/PageGenerator.js');
+const TeamDataCollector = require("./lib/TeamDataCollector.js");
+const PageGenerator = require("./lib/PageGenerator.js");
 
-new PageGenerator().init();
+const main = async () => {
+  try {
+    const employees = await new TeamDataCollector().init();
+    // PageGenerator
+    console.log("List of all employees", employees);
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+};
+
+main();

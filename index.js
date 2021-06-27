4339f20cd77784131ff7ee77df70024b3eb2bcaa
Alex <82488910+alexgorshkov02@users.noreply.github.com>
2021-06-27 00:18:09 -0400

    Added templates to build the final page. Added a logic to generate the page. Close #1 #8

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const TeamDataCollector = require("./lib/TeamDataCollector.js");
 const PageGenerator = require("./lib/PageGenerator.js");
+const PageTemplate = require("./lib/PageTemplate.js");
 
 const main = async () => {
   try {
-    const employees = await new TeamDataCollector().init();
-    // PageGenerator
-    console.log("List of all employees", employees);
+    const employeesList = await new TeamDataCollector().init();
+    console.log("List of all employees", employeesList);
+    
+    PageGenerator.writeToFile(employeesList);
+
   } catch (error) {
-    console.log(`Error: ${error}`);
+    if (error.isTtyError) {
+      console.log(
+        `Error: Prompt couldn't be rendered in the current environment`
+      );
+    } else {
+      console.log(`Error: ${error}`);
+    }
   }
 };
 
